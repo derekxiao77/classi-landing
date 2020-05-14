9729e33784f08bf47515c4738aa2ca5450712c53
Derek Xiao <derek.xiao77@gmail.com>
2020-05-14 01:26:54 -0400

    Updates

---
+++ precache-manifest.ac05c5d08a4c6b8665ab7491730a4c91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94caff999910dc9473a946f3353295fb",
+    "url": "/classi-landing/index.html"
+  },
+  {
+    "revision": "80f23f8773bf562329f3",
+    "url": "/classi-landing/static/css/main.a645b7b8.chunk.css"
+  },
+  {
+    "revision": "0597eaf33989bb4c3c06",
+    "url": "/classi-landing/static/js/2.97edf61c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/classi-landing/static/js/2.97edf61c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f23f8773bf562329f3",
+    "url": "/classi-landing/static/js/main.097b1afb.chunk.js"
+  },
+  {
+    "revision": "0560d3aa827cf61227dd",
+    "url": "/classi-landing/static/js/runtime-main.aa641469.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/classi-landing/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "913b927c526348b22adffd4ae20b4f02",
+    "url": "/classi-landing/static/media/overview_image.913b927c.png"
+  },
+  {
+    "revision": "6b75b45e108ca9cf0dc4556c0c08615d",
+    "url": "/classi-landing/static/media/overview_shape1.6b75b45e.svg"
+  },
+  {
+    "revision": "9fa80aa8bb2f4e14e12ec9ded9cde351",
+    "url": "/classi-landing/static/media/overview_shape2.9fa80aa8.png"
+  },
+  {
+    "revision": "3d6ca469d7e7b95799595a02fb33b1be",
+    "url": "/classi-landing/static/media/overview_shape3.3d6ca469.png"
+  }
+]);

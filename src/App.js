3c979385330161d1ddb68f5d6078af0b02911550
Derek Xiao <derek.xiao77@gmail.com>
2020-05-14 21:40:10 -0400

    student section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
+import './Images.css';
+const assets = require('./assets/assets.js')
     /*
   <div class="overview-section">
     <div class="left-align-content" style="position:absolute; width:315px; height:36px; top:125px; background:#FFFFFF;
@@ -13,6 +15,18 @@ import './App.css';
     <img src="assets/overview_image.png" alt="overview image" id="overview-mockup"/>
   </div>
   */
+
+// <div className = 'Tile-label'>{props.valueText}</div>
+function ValueTile(props) {
+    const tile = 
+        <div className = 'Value-tile'>
+            <div className="Tile-icon-container">
+                <img src={assets[props.imagePath]} alt="" className="Tile-icon"/>
+            </div>
+            <div className = 'Tile-label'>{props.valueText}</div>
+        </div>;
+    return tile;
+}
 
 function SubmitEmail() {
     const container =
@@ -83,8 +97,33 @@ function App() {
                 <img src={require("./assets/overview_image.png")} alt="" className="Classi-overview-image"/>
             </ContentSection>
             <ContentSection color="#FFF2F0">
+                <img src={require("./assets/overview_shape2.png")} alt="" className="Classi-student-shape2"/>
+                <img src={require("./assets/overview_shape3.png")} alt="" className="Classi-student-shape3"/>
+                <img src={require("./assets/overview_shape4.png")} alt="" className="Classi-student-shape4"/>
+                <img src={require("./assets/student_image.png")} alt="" className="Classi-student-image"/>
+                <div className = 'Student-content-container'>
+                    <h3>#WatchAClass</h3>
+                    <h2>Fitness is more fun with friends</h2>
+                    <span>Stay healthy and hang out with your friends at the same time. 
+                        Turn any live stream into a video chat by creating a private group
+                        and inviting your friends.
+                    </span>
+                    <div className="Selectable-tiles">
+                        <ValueTile imagePath={'friendsIcon'} valueText="Watch with friends"/>
+                        <ValueTile imagePath={'clapIcon'} valueText="Any live class. Free."/>
+                        <ValueTile imagePath={'praiseIcon'} valueText="Reach your fitness goals"/>
+                    </div>
+                </div>
             </ContentSection>
             <ContentSection color="#E7F4F6">
+                <div className = "Welcome-content-container">
+                    <h3>#WatchAClass</h3>
+                    <h2>Fitness is more fun with friends</h2>
+                    <span>Stay healthy and hang out with your friends at the same time. 
+                        Turn any live stream into a video chat by creating a private group
+                        and inviting your friends.
+                    </span>
+                </div>
             </ContentSection>
         </div>
     </div>
---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+const assetsObject = {
+    friendsIcon: require('./friends_icon.png'),
+    clapIcon: require('./clap_icon.png'),
+    praiseIcon: require('./praise_icon.png'),
+  };
+
+  module.exports = assetsObject
